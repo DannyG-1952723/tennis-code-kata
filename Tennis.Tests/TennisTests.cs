6b41888d05a10a12920a625d56e36b3972c8d523
Danny Grispen <danny.grispen@student.uhasselt.be>
2025-11-29 17:42:44 +0100

    Test: added test for incrementing points

---
+++ Tennis.Tests/TennisTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Tennis.Tests;
+
+[TestClass]
+public sealed class TennisTests
+{
+    [TestMethod]
+    [DataRow(Points.Love, Points.Fifteen)]
+    [DataRow(Points.Fifteen, Points.Thirty)]
+    [DataRow(Points.Thirty, Points.Forty)]
+    public void IncrementPoints_BasePoints(Points init, Points expected)
+    {
+        Points points = init;
+        points += 1;
+
+        Assert.AreEqual(expected, points);
+    }
+}
