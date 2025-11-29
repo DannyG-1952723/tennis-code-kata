c8e9f69561a7bbae8a342a2b8cfd416ffbc3e58a
Danny Grispen <danny.grispen@student.uhasselt.be>
2025-11-29 18:23:08 +0100

    Feat: implemented HasWon method

--- Tennis/Points.cs
+++ Tennis/Points.cs
@@ -5,7 +5,8 @@ public enum Points
     Love = 0,
     Fifteen = 1,
     Thirty = 2,
-    Forty = 3
+    Forty = 3,
+    Win = 4
 }
 
 public static class PointsExtension
@@ -13,5 +14,10 @@ public static class PointsExtension
     public static Points Increment(this Points points)
     {
         return points += 1;
+    }
+
+    public static bool HasWon(this Points points)
+    {
+        return points == Points.Win;
     }
 }
